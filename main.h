80a4f42b797974fd7b313d11da4c45845a1cac56
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-18 09:34:43 -0800

    wrbr.c

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stats.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+int wrbr(char c);
+
+#endif
