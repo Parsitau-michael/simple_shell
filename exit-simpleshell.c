94b04fd0dd6c1bf1d40df7b7db037382bd810577
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-28 04:58:30 -0800

    betty.c

---
+++ exit-simpleshell.c
@@ -0,0 +1,15 @@
+#include <stdli.h>
+#include <stdio.h>
+/**
+ * exiting - impliments the exit built in
+ */
+void exiting(void)
+{
+	shell_print("Exiting the shell")'
+		exit(0);
+}
+int main()
+{
+	exiting();
+	return 0;
+}
--- shell.h
+++ shell.h
@@ -16,22 +16,16 @@ int shell_print(char *s);
 int pass_betty(void);
 void prompt(void);
 char *user_inputs(void);
-<<<<<<< HEAD
-<<<<<<< HEAD
 char **tokenizer(char *s);
 int Betty(void);
-=======
 char **tokenize(char *s, char *delim);
->>>>>>> 5b27d066d683eb8d4632369a2b1da34d53461f47
 int exec(char **args);
 /* int exec(char **args);*/
-=======
 size_t word_len(char *str, char *delim);
 char **tokenize(char *s, char *delim);
 int execute(char **args);
 void print_env(void);
 void free_array(char **array);
->>>>>>> 421fe3d4b3578bb5ca3d7d346641dad58a28255b
 
 
 #endif /* SHELL_H */
